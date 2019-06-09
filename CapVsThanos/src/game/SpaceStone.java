3e5a4a02801855cee88781aa951ee43770977d54
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2019-06-09 16:41:06 -0300

    p.attack

--- CapVsThanos/src/game/SpaceStone.java
+++ CapVsThanos/src/game/SpaceStone.java
@@ -1,5 +1,14 @@
 package game;
 
-public class SpaceStone {
+public class SpaceStone extends Stone {
+	public SpaceStone()
+	{
+		
+	}
+	@Override
+	public int produceAttack() {
+		// TODO Auto-generated method stub
+		return super.getRan().nextInt()*80+70;
+	}
 
 }
