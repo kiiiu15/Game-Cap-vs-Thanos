2f9649ced840818017e83484d3ebcbe543a29c99
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2019-06-09 15:38:33 -0300

    constructor y tostring

--- CapVsThanos/src/game/InfinityGauntlet.java
+++ CapVsThanos/src/game/InfinityGauntlet.java
@@ -7,4 +7,19 @@ public class InfinityGauntlet extends Weapon {
 	{
 		this.infinityStone= new HashSet<Stone>();
 	}
+	@Override
+	public int causeHarm() {
+
+		return 0;//we have to modify the main method, it's incomplete. 
+	}
+	@Override
+	public int defence() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Infinity Gauntlet";
+	}
 }
