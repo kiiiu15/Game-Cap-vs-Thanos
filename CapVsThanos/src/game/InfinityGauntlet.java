ac196025af06b4a1760923a5ad87589259d8ffee
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2019-06-09 15:10:36 -0300

    constructor

--- CapVsThanos/src/game/InfinityGauntlet.java
+++ CapVsThanos/src/game/InfinityGauntlet.java
@@ -1,5 +1,10 @@
 package game;
+import java.util.*;
 
-public class InfinityGauntlet {
-
+public class InfinityGauntlet extends Weapon {
+	private HashSet<Stone>infinityStone;
+	public InfinityGauntlet()
+	{
+		this.infinityStone= new HashSet<Stone>();
+	}
 }
