d41009c994355144792ba885818e34a235e39f81
Kiiiu15 <kiiiu15@users.noreply.github.com>
2019-06-11 09:27:59 -0300

    In cap shield
    made equals better; i think

--- CapVsThanos/src/game/CapShield.java
+++ CapVsThanos/src/game/CapShield.java
@@ -16,4 +16,22 @@ public class CapShield extends Weapon {
 		return super.getDefenseCapacity();
 	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		boolean isEqual=false;
+		if (obj != null && obj instanceof CapShield) {
+			CapShield temporary = (CapShield) obj;
+			if (temporary.getDamage() == super.getDamage() && temporary.getDefenseCapacity()==super.getDefenseCapacity()) {
+				isEqual=true;
+			}
+		}
+		return isEqual;
+	}
+	
+	@Override
+	public String toString() {
+		String str=new String();
+		str="I´m Captian America´s shield, i didn´t broke even over Mjolnir but i just broke apart before thano´s wierd metal thing...";
+		return str;
+	}
 }
