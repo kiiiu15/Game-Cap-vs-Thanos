f4e2f967f0da4975f23d8a0691ddfc88754efd10
Kiiiu15 <kiiiu15@users.noreply.github.com>
2019-06-11 09:01:39 -0300

    In Stormbreaker
    ADDED JAVADOC

--- CapVsThanos/src/game/StormBreaker.java
+++ CapVsThanos/src/game/StormBreaker.java
@@ -1,5 +1,13 @@
 package game;
 
+
+/**
+ * 
+ * @author Agustin Bulzomi
+ * this i ment to be used as an object of type weapon for  any charater
+ * @see  Character
+ *  
+ */
 public class StormBreaker extends Weapon {
 	public StormBreaker() {
 		super();
