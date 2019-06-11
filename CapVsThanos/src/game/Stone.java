a714bbcce91909bc14247b85536e393682a6352a
Kiiiu15 <kiiiu15@users.noreply.github.com>
2019-06-11 09:19:17 -0300

    In stone
    added equals and toString

--- CapVsThanos/src/game/Stone.java
+++ CapVsThanos/src/game/Stone.java
@@ -23,6 +23,22 @@ public abstract class Stone {
 	}
 	
 	protected abstract int produceAtack();
+	
+	@Override
+		public boolean equals(Object obj) {
+			boolean isEqual=false;
+			if (obj != null && obj instanceof Stone) {
+				isEqual=true;
+			}
+			return isEqual;
+		}
+	
+	@Override
+		public String toString() {
+			String str=new String();
+			str="I´m the father of all infity stones";
+			return str;
+		}
 		
 		
 }
