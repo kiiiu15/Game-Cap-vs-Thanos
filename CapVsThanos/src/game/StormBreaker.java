c027a1d0bc71834f07aa31fad6f1cd76819026f3
Kiiiu15 <kiiiu15@users.noreply.github.com>
2019-06-11 08:38:24 -0300

    Implementation of abstractas methos in weapon class in storm breaker
    ass an heredated method an abstract had to implement since i ddon`t want my class to be abstratc too.

--- CapVsThanos/src/game/StormBreaker.java
+++ CapVsThanos/src/game/StormBreaker.java
@@ -1,5 +1,23 @@
 package game;
 
-public class StormBreaker {
+public class StormBreaker extends Weapon {
+	public StormBreaker() {
+		
+	}
 
+	@Override
+	public int causeHarm() {
+		// TODO Auto-generated method stub
+		return super.getDamage();
+	}
+
+	@Override
+	public int defence() {
+		// TODO Auto-generated method stub
+		return super.getDefenseCapacity();
+	}
+	
+	
+	
+	
 }
