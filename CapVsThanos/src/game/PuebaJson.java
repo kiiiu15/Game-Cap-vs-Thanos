198513fedfda1d788b1a1e79ac7b435dad19c258
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2019-06-14 14:23:34 -0300

    json obj thor

--- CapVsThanos/src/game/PuebaJson.java
+++ CapVsThanos/src/game/PuebaJson.java
@@ -1,5 +1,6 @@
 package game;
 import org.json.JSONString;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -9,6 +10,13 @@ import org.json.JSONObject;
 public class PuebaJson {//usamos array de personajes tambien.
 	public static void Main (String[] args)
 	{
+		Thor mithor=new Thor();
+		JSONObject myhero = new JSONObject();
+		myhero.put("Name",mithor.obtainName());
+		myhero.put("Life", mithor.thorLife());
+		
+		JSONArray listOfCharacters = new JSONArray();
+		
 	
 	}
 
