c4ef69a0eb39a35a27b5cc7c726dbbecd6d9d72e
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2019-06-14 12:40:29 -0300

    Json

---
+++ CapVsThanos/src/game/PuebaJson.java
@@ -0,0 +1,16 @@
+package game;
+import org.json.simple.JSONArray;
+import org.json.JSONString;
+import org.json.JSONObject;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.json.JSONObject;
+
+public class PuebaJson {//usamos array de personajes tambien.
+	public static void Main (String[] args)
+	{
+	
+	}
+
+}
