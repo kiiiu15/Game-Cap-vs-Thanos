46883fea0939445dd2b231be141c7770d3781410
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2019-06-14 14:34:39 -0300

    comment for ambition

--- CapVsThanos/src/game/Villain.java
+++ CapVsThanos/src/game/Villain.java
@@ -6,10 +6,11 @@ package game;
  */
 public class Villain extends Character{
 
-	private int ambition;
+	private int ambition;// there should be an abs class so thanos can use it
 	
 	public Villain()
 	{
 		super();
 	}
+
 }
