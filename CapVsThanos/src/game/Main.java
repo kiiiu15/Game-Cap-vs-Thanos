038aa31cb033d59ff6ee1fddef681c5cc39428a9
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2019-06-15 02:56:30 -0300

    main changes

--- CapVsThanos/src/game/Main.java
+++ CapVsThanos/src/game/Main.java
@@ -22,29 +22,37 @@ public class Main {
 	public static void main(String[] args) {
 		
 		ArrayList<Hero>listOfHeroes = new ArrayList<>();
-			
-		
 		
 		Hero hero=new Hero("Thor", 100);
 		
-		JSONObject myhero = new JSONObject();
+		JSONObject myheroes = new JSONObject();
 		try {
-			myhero.put("Name",hero.obtainName());
+			myheroes.put("Name",hero.obtainName());
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}// error for exception 
 		try {
-			myhero.put("Life", hero.obtainLife());
+			myheroes.put("Life", hero.obtainLife());
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		listOfHeroes.add(hero);
+		for(Hero heroes : listOfHeroes)
+		{
+			listOfHeroes.add(heroes);
+		}
+		try {
+			myheroes.put("Marvel's Heroes",listOfHeroes);
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String output=myheroes.toString();
+		System.out.print(output);
 		
-		ArrayList<Hero>ListOfVillain = new ArrayList<>();
+		ArrayList<Villain>ListOfVillain = new ArrayList<>();
 		Villain villain= new Villain("Thanos", 100);
-		ListOfVillain.add(Villain );
 		JSONObject myVillain = new JSONObject();
 		try {
 			myVillain.put("Name", villain.obtainName());
@@ -58,13 +66,24 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-				
+		for(Villain villians : ListOfVillain)
+		{
+			ListOfVillain.add(villians);
+		}
+		try {
+			myVillain.put("Marvel's Villains", ListOfVillain);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String exit=myVillain.toString();
+		System.out.print(exit);
 		
-		JSONArray listOfCharacters = new JSONArray();
+		/*JSONArray listOfCharacters = new JSONArray();
 		listOfCharacters.put(myhero);
 		listOfCharacters.put(myVillain);
 		System.out.print(myhero);
-		System.out.print(myVillain);
+		System.out.print(myVillain);*/
      		
 	  Welcome welcome = new Welcome();
 	  welcome.setBounds(0,0,1000,700);
