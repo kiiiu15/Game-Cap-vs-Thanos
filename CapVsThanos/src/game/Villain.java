cbfb7fcf2f0a484a406365c2d1d4add7df99bc39
tinch0fernandez <44264325+tinch0fernandez@users.noreply.github.com>
2019-06-14 22:57:43 -0300

    .

--- CapVsThanos/src/game/Villain.java
+++ CapVsThanos/src/game/Villain.java
@@ -6,11 +6,35 @@ package game;
  */
 public class Villain extends Character{
 
-	private int ambition;// there should be an abs class so thanos can use it
+	private int ambition; 
 	
 	public Villain()
 	{
 		super();
+	}
+
+	@Override
+	public int atack() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int defendYourSelf(int protection) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void getAtacked(int damageRecieved) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int lifeLeft() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
