32f83e1915112f644015c80666d3e23e17a9db62
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2019-06-15 03:56:52 -0300

    Update Main.java

--- CapVsThanos/src/game/Main.java
+++ CapVsThanos/src/game/Main.java
@@ -21,54 +21,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Hero>listOfHeroes = new ArrayList<>();
 		
-		Hero hero=new Hero("Thor", 80);
-		
-		JSONObject myheroes = new JSONObject();
-		try {
-			myheroes.put("Name",hero.obtainName());
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}// error for exception 
-		try {
-			myheroes.put("Life", hero.obtainLife());
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		for(Hero heroes : listOfHeroes)
-		{
-			listOfHeroes.add(heroes);
-		}
-		try {
-			myheroes.put("Marvel's Heroes",listOfHeroes);
-		} catch (JSONException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		String output=myheroes.toString();
-		System.out.print(output);
-		
-		Villain villain= new Villain("Thanos", 90);
-		
-		JSONObject myVillain = new JSONObject();
-		try {
-			myVillain.put("Name", villain.obtainName());
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			myVillain.put("Life", 90);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		String exit=myVillain.toString();
-		System.out.println(exit);
 		
      		
 	  Welcome welcome = new Welcome();
